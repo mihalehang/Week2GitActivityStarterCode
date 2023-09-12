154a46031855892e964ab6bfe6dbd513715bd177
Michael Huang <michael.s.huang.01@gmail.com>
2023-09-12 16:38:51 -0400

    added multiply function

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
+const result_mult = calc.multiply(2, 3);
+
 console.log(`result_add = ${result_add}`);
+console.log(`result_mult = ${result_mult}`)
 
