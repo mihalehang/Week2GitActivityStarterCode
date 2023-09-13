ce5e7e2224fc3a5bdbe332fc661d2c5eb59edec8
Michael Huang <michael.s.huang.01@gmail.com>
2023-09-13 12:27:41 -0400

    mult

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 const result_sub = calc.subtract(3, 2);
+const result_mult = calc.mult(3, 2);
+
 
 console.log(`result_add = ${result_add}`);
 console.log(`result_sub = ${result_sub}`);
+console.log(`result_mult = ${result_mult}`);
 
