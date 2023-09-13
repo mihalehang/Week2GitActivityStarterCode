e5ed071b21a6a2c0590695f1f05f8f40cb28e881
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2023-09-13 12:53:20 -0400

    "new divide operation

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 const result_sub = calc.subtract(3, 2);
 const result_mult = calc.multiply(3, 2);
+const result_div = calc.divide(4,2);
 
+console.log(`result_mult = ${result_mult}`);
 console.log(`result_add = ${result_add}`);
 console.log(`result_sub = ${result_sub}`);
-console.log(`result_mult = ${result_mult}`);
+console.log(`result_div = ${result_div}`);
